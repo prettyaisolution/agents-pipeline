efdba3ace78e97f68fc551a023ae37da698aae55
Vasyl Herman <vasil.german@gmail.com>
2026-06-06 15:37:50 +0300

    init

--- lib/agents-pipeline-stack.ts
+++ lib/agents-pipeline-stack.ts
@@ -9,16 +9,16 @@ export class AgentsPipelineStack extends cdk.Stack {
     constructor(scope: Construct, id: string, props?: cdk.StackProps) {
         super(scope, id, props);
 
-        const container = new DockerImageAsset(this, 'Container', {
-            assetName: "agentName",
-            directory: `../myproject/app/MyAgent2`,
+        const container = new DockerImageAsset(this, 'MyAgent2C', {
+            assetName: "MyAgent2",
+            directory: `../MyAgent2`,
             ignoreMode: IgnoreMode.GLOB,
             platform: Platform.LINUX_ARM64,
         })
 
         const agentRuntimeArtifact = AgentRuntimeArtifact.fromImageUri(container.imageUri)
 
-        const myAgent2 = new Runtime(this, "MyAgent2", {
+        const myAgent2 = new Runtime(this, "MyAgent2R", {
             runtimeName: "MyAgent2",
             agentRuntimeArtifact: agentRuntimeArtifact,
 
